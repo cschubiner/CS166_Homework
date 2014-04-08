39a911daaec736a03592eeb03de35730a1b768f7
Clay Schubiner <cschubiner@gmail.com>
2014-04-07 19:03:11 -0700

    top layer

--- ps1/rmq/HybridRMQ.java
+++ ps1/rmq/HybridRMQ.java
@@ -11,7 +11,29 @@ public class HybridRMQ implements RMQ {
     int b;
 
     SparseTableRMQ topLayerRMQ;
-    int[] topLayerBlocks;
+    float[] topLayerBlocks;
+
+
+    private void createTopLayer(float[] elems) {
+        float minVal = Float.MAX_VALUE;
+        int numBlocks = elems.length / b;
+        if (elems.length % b > 0) numBlocks++;
+        topLayerBlocks = new float[numBlocks];
+        int blockIndex = 0;
+        for (int i = 0; i < elems.length; i++) {
+            if (i % b == 0 && i > 0) {
+                blockIndex++;
+                minVal = Float.MAX_VALUE;
+            }
+            if (elems[i] < minVal) {
+                minVal = elems[i];
+                topLayerBlocks[blockIndex] = minVal;
+            }
+        }
+
+        //Now that topLayerBlocks is filled, we'll create the sparse table that sits atop it
+        topLayerRMQ = new SparseTableRMQ(topLayerBlocks);
+    }
 
     /**
      * Creates a new HybridRMQ structure to answer queries about the
@@ -20,7 +42,10 @@ public class HybridRMQ implements RMQ {
      * @elems The array over which RMQ should be computed.
      */
     public HybridRMQ(float[] elems) {
-        // TODO: Implement this!
+        elems = new float[]{31,41,59,26,53,58,97,93,23,84,62,64,33,27};
+        int elems_length = elems.length;
+        b = (int)(Math.log(elems_length)/Math.log(2));
+        createTopLayer(elems);
     }
 
     /**
