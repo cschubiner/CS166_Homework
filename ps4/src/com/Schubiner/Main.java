5386cfbea3b00e44bcca22d4d4f4bd298567115b
Clay Schubiner <cschubiner@gmail.com>
2014-04-28 17:17:21 -0700

    hi

---
+++ ps4/src/com/Schubiner/Main.java
@@ -0,0 +1,20 @@
+package com.Schubiner;
+
+import org.junit.Test;
+
+
+import static org.junit.Assert.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+
+    @Test
+    public void testEasy() {
+    LazyBinomialHeap lbh = new LazyBinomialHeap();
+        lbh.enqueue(2);
+        assertEquals(2, lbh.min()); //we want 2
+    }
+}
---
+++ ps4/src/com/Schubiner/MeldableLinkedList.java
@@ -0,0 +1,10 @@
+package com.Schubiner;
+
+/**
+ * A doubly-linked list that supports O(1) concatenation. You are not required
+ * to implement this class, but we strongly recommend doing so as a stepping
+ * stone toward building the LazyBinomialHeap.
+ */
+public class MeldableLinkedList {
+    // TODO: Implement this class as you see fit!
+}
